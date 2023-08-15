3eba2c6e08baff2774d7fb7de6943ed3d1bc7e8f
kotoslav <zalll007@yandex.ru>
2023-08-15 08:23:58 +0300

    script.js builded

--- frontend/js/callbackFormIndex.js
+++
@@ -1,1 +0,0 @@
-import{CallbackForm}from"./callbackForm.js";class CallbackFormIndex extends CallbackForm{constructor(){super(),this.form.classList.add("_anim-items"),this.form.setAttribute("id","form-animation")}}export{CallbackFormIndex};
